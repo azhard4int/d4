a2f73db22e9d0c5320d263fa61131ca16fdc674a
azhar <azhar@d4int.com>
2016-01-23 23:52:27 +0500

    Just to sync the database.

--- accounts/models.py
+++ accounts/models.py
@@ -74,8 +74,9 @@ class Commands(models.Model):
     vm = models.ForeignKey(VMS, max_length=64, null=False, default=None)
     action = models.TextField(null=False, default=None)
     executed = models.BooleanField(default=False)
+    failed = models.BooleanField(default=False)
 
 
 class Errors(models.Model):
-    client = models.ForeignKey(User, default=None)
+    command = models.ForeignKey(Commands, default=None)
     message = models.TextField(default=None)
